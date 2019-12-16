94f224ab70bf730b8a068e965e6c5bd9a8559c16
Linus Lee <linus@thesephist.com>
2019-12-15 23:56:37 -0800

    Start work on frontned

--- tpl/js/main.js
+++ tpl/js/main.js
@@ -1,1 +1,19 @@
-console.log('Welcome to nought');
+const {
+    StyledComponent,
+    router,
+} = window.Torus;
+
+class App extends StyledComponent {
+    init() {
+
+    }
+    compose() {
+        return jdom`<div class="app">
+            Hello from nought!
+            <button>click me</button>
+        </div>`;
+    }
+}
+
+const app = new App();
+document.getElementById('app').appendChild(app.node);
